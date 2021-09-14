913a24bffa64e58496e7432179a77afceae4c022
Lisa Bost <bost.lisam@gmail.com>
2021-09-14 13:41:33 -0500

    Display all blogs update homecontroller update index view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
+using Blogs.Models;
 
 namespace Blogs.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index() => View();
+        // this controller depends on the BloggingRepository
+        private BloggingContext _bloggingContext;
+        public HomeController(BloggingContext db) => _bloggingContext = db;
+
+        public IActionResult Index() => View(_bloggingContext.Blogs);
     }
 }
